6824b3d374724116320a658df995d0416e69795f
Nick McCoy <nick@stitchdata.com>
2018-10-19 19:02:38 +0000

    add products and fix other schemas

--- tap_shopify/streams/__init__.py
+++ tap_shopify/streams/__init__.py
@@ -3,3 +3,4 @@ import tap_shopify.streams.customers
 import tap_shopify.streams.orders
 import tap_shopify.streams.metafields
 import tap_shopify.streams.transactions
+import tap_shopify.streams.products
