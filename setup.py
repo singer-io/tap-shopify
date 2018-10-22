6f99e8801fd608ee87c37c0c422289d5ec2b9175
Nick McCoy <nick@stitchdata.com>
2018-10-22 13:34:39 +0000

    bump version to 0.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-shopify",
-    version="0.3.0",
+    version="0.3.1",
     description="Singer.io tap for extracting Shopify data",
     author="Stitch",
     url="http://github.com/singer-io/tap-shopify",
