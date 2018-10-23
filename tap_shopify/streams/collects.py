bec760462f58b041851d49a93ad2c164db3e1cec
Nick McCoy <33731945+nick-mccoy@users.noreply.github.com>
2018-10-23 16:49:10 -0400

    Bookmarking changes (#9)
    * update datetime comparisons
    
    * formatting;pylint

--- tap_shopify/streams/collects.py
+++ tap_shopify/streams/collects.py
@@ -1,5 +1,5 @@
-import singer
 import shopify
+from singer import utils
 from tap_shopify.streams.base import (Stream,
                                       RESULTS_PER_PAGE)
 from tap_shopify.context import Context
@@ -8,29 +8,29 @@ from tap_shopify.context import Context
 class Collects(Stream):
     name = 'collects'
     replication_object = shopify.Collect
-    replication_key = 'id'
+    replication_key = 'updated_at'
 
     def get_objects(self):
         page = 1
-        start_id = singer.get_bookmark(Context.state,
-                                       self.name,
-                                       self.replication_key)
-
+        bookmark = self.get_bookmark()
+        max_bookmark = utils.strftime(utils.now())
         while True:
             query_params = {
                 "page": page,
                 "limit": RESULTS_PER_PAGE,
             }
-            if start_id:
-                query_params["since_id"] = start_id
 
             objects = self.call_api(query_params)
 
             for obj in objects:
-                yield obj
-                self.update_bookmark(obj)
+                # Syncing Collects is a full sync every time but emitting records that have
+                # an updated_date greater than the bookmark
+                if utils.strptime_with_tz(obj.updated_at) > bookmark:
+                    yield obj
 
             if len(objects) < RESULTS_PER_PAGE:
+                # Update the bookmark at the end of the last page
+                self.update_bookmark(max_bookmark)
                 break
             page += 1
 
