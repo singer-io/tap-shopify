3255b83aec09b42d972fa002f0d066c57b346133
Tim Visher <tim@stitchdata.com>
2018-11-09 19:45:51 +0000

    0.6.3 → 0.6.4

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-shopify",
-    version="0.6.3",
+    version="0.6.4",
     description="Singer.io tap for extracting Shopify data",
     author="Stitch",
     url="http://github.com/singer-io/tap-shopify",
