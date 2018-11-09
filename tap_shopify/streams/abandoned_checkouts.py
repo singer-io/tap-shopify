997427b4c1fadf885e7fb4dfb0e4bc2c4f10f454
Kyle Allan <kyle@stitchdata.com>
2018-11-09 11:53:43 -0500

    wip abandoned checkouts

--- tap_shopify/streams/abandoned_checkouts.py
+++ tap_shopify/streams/abandoned_checkouts.py
@@ -6,13 +6,4 @@ class AbandonedCheckouts(Stream):
     name = 'abandoned_checkouts'
     replication_object = shopify.Checkout
 
-    def sync(self):
-        for abandoned_checkout in self.get_objects():
-            abandoned_checkout_dict = abandoned_checkout.to_dict()
-            # Customer is a stream on its own that can be foreign keyed to
-            if abandoned_checkout_dict.get('customer'):
-                customer = abandoned_checkout_dict.pop('customer')
-                abandoned_checkout_dict['customer_id'] = customer['id']
-            yield abandoned_checkout_dict
-
 Context.stream_objects['abandoned_checkouts'] = AbandonedCheckouts
