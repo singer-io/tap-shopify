bb9044fc3c5bab5d5b29bbcc48dec6a1ca3d019a
Siddharth Sudheer <siddharthsudheer@gmail.com>
2019-04-03 15:40:32 -0400

    made abandoned checkouts async capable

--- tap_shopify/streams/abandoned_checkouts.py
+++ tap_shopify/streams/abandoned_checkouts.py
@@ -5,5 +5,8 @@ from tap_shopify.streams.base import Stream, RunAsync
 class AbandonedCheckouts(Stream):
     name = 'abandoned_checkouts'
     replication_object = shopify.Checkout
+    endpoint = "/checkouts"
+    result_key = "checkouts"
+    async_available = True
 
 Context.stream_objects['abandoned_checkouts'] = AbandonedCheckouts
