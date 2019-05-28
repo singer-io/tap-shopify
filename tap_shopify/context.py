42cc8b69d3a62274a305afeef8e223cf02bf07ff
Clayton Eu <clayton@wisepipe.com>
2019-05-28 17:28:15 -0400

    abandoned_checkouts and metafields no longer crashes

--- tap_shopify/context.py
+++ tap_shopify/context.py
@@ -12,10 +12,14 @@ class Context():
     def get_catalog_entry(cls, stream_name):
         if not cls.stream_map:
             cls.stream_map = {s["tap_stream_id"]: s for s in cls.catalog['streams']}
-        return cls.stream_map[stream_name]
+
+        return cls.stream_map[stream_name] if stream_name in cls.stream_map else None
 
     @classmethod
     def is_selected(cls, stream_name):
         stream = cls.get_catalog_entry(stream_name)
+        if(stream is None): 
+            return False
+
         stream_metadata = metadata.to_map(stream['metadata'])
         return metadata.get(stream_metadata, (), 'selected')
