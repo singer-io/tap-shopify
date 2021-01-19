fb380af721ecd5eae339cfdbebcc80d727dd6ed1
mohamedfawzy96 <mohamedfawzydes@gmail.com>
2021-01-19 15:55:59 +0100

    adding max value for sleeping

--- tap_shopify/streams/base.py
+++ tap_shopify/streams/base.py
@@ -66,14 +66,17 @@ def shopify_error_handling(fnc):
                            simplejson.scanner.JSONDecodeError),
                           on_backoff=retry_handler,
                           max_tries=MAX_RETRIES,
-                          jitter=None)
+                          jitter=None,
+                          max_value=4)
     @backoff.on_exception(backoff.expo,
                           (pyactiveresource.connection.ClientError,
                            pyactiveresource.connection.Error),
                           giveup=is_not_status_code_fn([429]),
                           on_backoff=leaky_bucket_handler,
                           # No jitter as we want a constant value
-                          jitter=None)
+                          jitter=None,
+                          max_value=4
+                          )
     @functools.wraps(fnc)
     def wrapper(*args, **kwargs):
         return fnc(*args, **kwargs)
@@ -237,7 +240,7 @@ class Stream():
             while True:
                 query = self.get_graph_query(updated_at_min,
                                              updated_at_max,
-                                             100,
+                                             150,
                                              child,
                                              child_parameters,
                                              results_per_page,
