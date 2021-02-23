28ba615a39fad1516c853403de552cd2c9008e9f
Collin Simon <csimon@talend.com>
2021-02-23 21:05:43 +0000

    Put error message on a single line

--- tap_shopify/exceptions.py
+++ tap_shopify/exceptions.py
@@ -1,3 +1,3 @@
 class ShopifyError(Exception):
     def __init__(self, error, msg=''):
-        super().__init__('{}\n{}'.format(error.__class__.__name__, msg))
+        super().__init__('{}: {}'.format(error.__class__.__name__, msg))
