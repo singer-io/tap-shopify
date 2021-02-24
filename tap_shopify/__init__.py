33b27ceb393bd6380cdc76f99e2c44201307b234
Collin Simon <csimon@talend.com>
2021-02-24 15:17:24 +0000

    Change top level exception logger to log lines individually

--- tap_shopify/__init__.py
+++ tap_shopify/__init__.py
@@ -173,27 +173,30 @@ def sync():
         LOGGER.info('%s: %d', stream_id, stream_count)
     LOGGER.info('----------------------')
 
-@utils.handle_top_exception(LOGGER)
 def main():
+    try:
+        # Parse command line arguments
+        args = utils.parse_args(REQUIRED_CONFIG_KEYS)
 
-    # Parse command line arguments
-    args = utils.parse_args(REQUIRED_CONFIG_KEYS)
-
-    # If discover flag was passed, run discovery mode and dump output to stdout
-    if args.discover:
-        catalog = discover()
-        print(json.dumps(catalog, indent=2))
-    # Otherwise run in sync mode
-    else:
-        Context.tap_start = utils.now()
-        if args.catalog:
-            Context.catalog = args.catalog.to_dict()
+        # If discover flag was passed, run discovery mode and dump output to stdout
+        if args.discover:
+            catalog = discover()
+            print(json.dumps(catalog, indent=2))
+        # Otherwise run in sync mode
         else:
-            Context.catalog = discover()
+            Context.tap_start = utils.now()
+            if args.catalog:
+                Context.catalog = args.catalog.to_dict()
+            else:
+                Context.catalog = discover()
 
-        Context.config = args.config
-        Context.state = args.state
-        sync()
+            Context.config = args.config
+            Context.state = args.state
+            sync()
+    except Exception as e:
+        for line in str(e).splitlines():
+            LOGGER.critical(line)
+        raise e
 
 if __name__ == "__main__":
     main()
