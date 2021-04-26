6e3cf3e6869854d7467a30165e6a07f85e1e0516
Andy Lu <andy@stitchdata.com>
2021-04-26 15:45:57 +0000

    Bump to v1.3.0, update changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-shopify",
-    version="1.2.10",
+    version="1.3.0",
     description="Singer.io tap for extracting Shopify data",
     author="Stitch",
     url="http://github.com/singer-io/tap-shopify",
