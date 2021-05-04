024cc9da741a782cb8a26254d019ef610915d9a9
kchan-varicent <63657700+kchan-varicent@users.noreply.github.com>
2021-05-04 14:33:22 -0400

    WP-6215: Relaxed install_requires versions, added package management. (#2)

--- setup.py
+++ setup.py
@@ -3,15 +3,15 @@ from setuptools import setup
 
 setup(
     name="tap-shopify",
-    version="1.1.10",
+    version="1.1.12",
     description="Singer.io tap for extracting Shopify data",
     author="Stitch",
     url="http://github.com/singer-io/tap-shopify",
     classifiers=["Programming Language :: Python :: 3 :: Only"],
     py_modules=["tap_shopify"],
     install_requires=[
-        "ShopifyAPI==3.1.0",
-        "singer-python==5.4.1",
+        "ShopifyAPI~=3.1.0",
+        "singer-python~=5.4.1",
     ],
     extras_require={
         'dev': [
