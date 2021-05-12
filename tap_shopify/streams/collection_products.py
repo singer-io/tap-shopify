45c61cb870721995e8deb6d677d8fd5c68fa1f7b
Joel Hendy <joel.hendy@lexer.com.au>
2021-05-12 15:41:25 +1000

    pull down collection products

---
+++ tap_shopify/streams/collection_products.py
@@ -0,0 +1,30 @@
+import shopify
+
+from tap_shopify.context import Context
+from tap_shopify.streams.base import (Stream,
+                                      RESULTS_PER_PAGE,
+                                      shopify_error_handling,
+                                      OutOfOrderIdsError)
+
+
+class CollectionProducts(Stream):
+    name = 'collection_products'
+    replication_object = shopify.CustomCollection
+    replication_key = 'updated_at'
+
+    def get_objects(self):
+        
+        while True:
+            for collection in replication_object.find():
+                for product in collection.products():
+                    yield product
+            if collection.has_next_page():
+                collection = collection.next_page()
+            else:
+                break
+
+    def sync(self):
+        for product in self.get_objects():
+            yield product.to_dict()
+
+Context.stream_objects['collection_products'] = CollectionProducts
