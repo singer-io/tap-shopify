88d62424c1dd8b64f3131c679781f6d5211c93bd
Joel Hendy <joel.hendy@lexer.com.au>
2021-06-18 12:55:56 +1000

    paginate through products also

--- tap_shopify/streams/custom_collections_products.py
+++ tap_shopify/streams/custom_collections_products.py
@@ -15,13 +15,22 @@ class CustomCollectionsProducts(Stream):
     def get_objects(self):
         'Paginate the return and add collection_id to returned product objects'
         page = self.replication_object.find()
-        for collection in page:
-            for product in collection.products():
-                edit_product = product.to_dict()
-                edit_product["collection_id"] = collection.id
-                yield edit_product
-        if page.has_next_page():
-            page = page.next_page()
+        while True:
+            for collection in page:
+                collection_products_page = collection.products()
+                while True:
+                    for product in collection_products_page:
+                        edit_product = product.to_dict()
+                        edit_product["collection_id"] = collection.id
+                        yield edit_product
+                    if collection_products_page.has_next_page():
+                        collection_products_page = collection_products_page.next_page()
+                    else:
+                        break
+            if page.has_next_page():
+                page = page.next_page()
+            else:
+                break
 
     def sync(self):
         for product in self.get_objects():
--- tap_shopify/streams/smart_collections_products.py
+++ tap_shopify/streams/smart_collections_products.py
@@ -6,22 +6,31 @@ from tap_shopify.streams.base import (Stream,
                                       shopify_error_handling,
                                       OutOfOrderIdsError)
 
-
 class SmartCollectionsProducts(Stream):
     name = 'smart_collections_products'
     replication_object = shopify.SmartCollection
     replication_key = 'updated_at'
 
+
     def get_objects(self):
         'Paginate the return and add collection_id to returned product objects'
         page = self.replication_object.find()
-        for collection in page:
-            for product in collection.products():
-                edit_product = product.to_dict()
-                edit_product["collection_id"] = collection.id
-                yield edit_product
-        if page.has_next_page():
-            page = page.next_page()
+        while True:
+            for collection in page:
+                collection_products_page = collection.products()
+                while True:
+                    for product in collection_products_page:
+                        edit_product = product.to_dict()
+                        edit_product["collection_id"] = collection.id
+                        yield edit_product
+                    if collection_products_page.has_next_page():
+                        collection_products_page = collection_products_page.next_page()
+                    else:
+                        break
+            if page.has_next_page():
+                page = page.next_page()
+            else:
+                break
 
     def sync(self):
         for product in self.get_objects():
