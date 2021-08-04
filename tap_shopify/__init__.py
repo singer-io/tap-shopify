9f44ddc5b91334c0eab7e44e3fa5ce73e7fe0ee6
savan-chovatiya <savan.chovatiya@crestdatasys.com>
2021-08-04 14:21:38 +0530

    Resolved pylint failure

--- tap_shopify/__init__.py
+++ tap_shopify/__init__.py
@@ -157,7 +157,9 @@ def sync():
                 raise ShopifyError(exc, 'Invalid access token - Re-authorize the connection') \
                     from exc
             except pyactiveresource.connection.ForbiddenAccess as exc:
-                raise ShopifyError(exc, 'Add read_inventory scope for access token and Re-authorize the connection to sync Inventory Levels and Inventory Items streams.') \
+                raise ShopifyError(exc, 'Add read_inventory scope for access token and '
+                                   'Re-authorize the connection to sync '
+                                   'Inventory Levels and Inventory Items streams.') \
                     from exc
             except pyactiveresource.connection.ConnectionError as exc:
                 msg = ''
