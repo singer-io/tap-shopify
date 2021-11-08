089a9454b4ea8442758c46aaf0df37992d89417f
Andy Lu <andy@stitchdata.com>
2021-11-08 16:11:29 -0500

    Make pylint happy

--- tap_shopify/streams/events.py
+++ tap_shopify/streams/events.py
@@ -9,11 +9,11 @@ class Events(Stream):
     replication_object = shopify.Event
     replication_key = "created_at"
 
-    def get_query_params(self, since_id, status_key, created_at_min, created_at_max):
+    def get_query_params(self, since_id, status_key, updated_at_min, updated_at_max):
         return {
             "since_id": since_id,
-            "created_at_min": created_at_min,
-            "created_at_max": created_at_max,
+            "created_at_min": updated_at_min,
+            "created_at_max": updated_at_max,
             "limit": self.results_per_page,
             status_key: "any"
         }
