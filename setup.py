09a5b31cd076a558f790778e3f1449836e50d955
Joel Hendy <joel.hendy@lexer.com.au>
2022-02-03 14:49:31 +1100

    bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-shopify",
-    version="1.2.6.7",
+    version="1.2.6.8",
     description="Singer.io tap for extracting Shopify data",
     author="Stitch",
     url="http://github.com/singer-io/tap-shopify",
