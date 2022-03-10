6776cdc1fbadb296640f36f2d82c6d6f3b09dc99
“n-xara” <najada.xara@y42.com>
2022-03-10 16:24:38 +0100

    add customer_id as separate column on orders

--- tap_shopify/streams/orders.py
+++ tap_shopify/streams/orders.py
@@ -8,5 +8,16 @@ class Orders(Stream):
     name = 'orders'
     replication_object = shopify.Order
 
+    def sync(self):
+        """Yield's processed SDK object dicts to the caller.
+
+        This is the default implementation. Get's all of self's objects
+        and calls to_dict on them with no further processing.
+        """
+        for obj in self.get_objects():
+            record = obj.to_dict()
+            record["customer_id"] = record["customer"] and record["customer"]["id"] or False
+            yield record
+
 
 Context.stream_objects['orders'] = Orders
