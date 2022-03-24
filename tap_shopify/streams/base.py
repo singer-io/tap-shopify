17c361ba8d79d8438353bb91c0c1e6b8d1a5b199
namrata270998 <namrata.brahmbhatt@crestdatasys.com>
2022-03-24 12:33:58 +0000

    test commit

--- tap_shopify/streams/base.py
+++ tap_shopify/streams/base.py
@@ -135,7 +135,8 @@ class Stream():
         return utils.strptime_with_tz(bookmark)
 
     def get_since_id(self):
-        LOGGER.info(f'>>>>>> state {Context.state}')
+        LOGGER.info('>>>>>> state')
+        LOGGER.info(Context.state)
         return singer.get_bookmark(Context.state,
                                    # name is overridden by some substreams
                                    self.name,
