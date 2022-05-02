492d9a0ac5a1d56715d851fac9196a8d580f6447
KrisPersonal <66801357+KrisPersonal@users.noreply.github.com>
2022-05-02 14:15:07 -0700

    Fix transform Issues (#150)
    Co-authored-by: KrishnanG <kgurusamy@talend.com>

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-shopify",
-    version="1.6.0",
+    version="1.6.1",
     description="Singer.io tap for extracting Shopify data",
     author="Stitch",
     url="http://github.com/singer-io/tap-shopify",
