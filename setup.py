cefc92a131e628ad12f9ba4a6970a8f84b293c55
dsprayberry <28106103+dsprayberry@users.noreply.github.com>
2022-11-30 18:24:31 +0000

    Version Bump and Changelog

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="tap-shopify",
-    version="1.6.1",
+    version="1.6.2",
     description="Singer.io tap for extracting Shopify data",
     author="Stitch",
     url="http://github.com/singer-io/tap-shopify",
