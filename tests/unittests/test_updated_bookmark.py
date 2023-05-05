0156a80b5ad5bdce5d413b0edaa620aeda13adbc
RushiT0122 <rtodkar@stitchdata-talend.com>
2023-05-05 08:32:17 +0000

    add unit tests

--- tests/unittests/test_updated_bookmark.py
+++ tests/unittests/test_updated_bookmark.py
@@ -19,8 +19,8 @@ class Customer():
         return {"id": self.id, "updated_at": self.updated_at}
 
 
-CUSTOMER_1 = Customer(2, "2021-08-11T01:57:05-04:00")
-CUSTOMER_2 = Customer(3, "2021-08-12T01:57:05-04:00")
+CUSTOMER_1 = Customer(20, "2021-08-11T01:57:05-04:00")
+CUSTOMER_2 = Customer(30, "2021-08-12T01:57:05-04:00")
 
 NOW_TIME = '2021-08-16T01:56:05-04:00'
 class TestUpdateBookmark(unittest.TestCase):
@@ -37,3 +37,53 @@ class TestUpdateBookmark(unittest.TestCase):
         customers = list(CUSTOMER_OBJECT.get_objects())
         # Verify that the min-max evaluation returns correct results and provided in the update_bookmark()
         mock_update_bookmark.assert_called_with(strftime(strptime_to_utc(NOW_TIME) - datetime.timedelta(DATE_WINDOW_SIZE)))
+
+
+    @mock.patch("tap_shopify.streams.base.Stream.call_api")
+    @mock.patch("tap_shopify.streams.base.Stream.get_bookmark", return_value=strptime_to_utc('2021-08-11T01:55:05-04:00'))
+    @mock.patch("tap_shopify.streams.base.Stream.update_bookmark")
+    @mock.patch("tap_shopify.streams.base.Stream.get_query_params")
+    @mock.patch("singer.utils.now", return_value=strptime_to_utc(NOW_TIME))
+    def test_since_id_and_updated_at_max_deleted(self, mock_now, mock_get_query_params, mock_update_bookmark, mock_get_bookmark, mock_call_api):
+        """Verify after successful sync since_id and updated_at_max keys are deleted from the state"""
+        mock_call_api.return_value = [CUSTOMER_1, CUSTOMER_2]
+        Context.state = {"bookmarks": {
+            "currently_sync_stream": 'customers',
+            "customers": {
+                "updated_at": "2021-03-27T00:00:00.000000Z",
+                "since_id": 15,
+                "updated_at_max": "2021-04-26T00:00:00.000000Z"}}}
+
+        customers = list(CUSTOMER_OBJECT.get_objects())
+
+        self.assertNotIn("since_id", Context.state["bookmarks"]["customers"])
+        self.assertNotIn("updated_at_max", Context.state["bookmarks"]["customers"])
+
+    @mock.patch("tap_shopify.streams.base.Stream.call_api")
+    @mock.patch("tap_shopify.streams.base.Stream.get_bookmark", return_value=strptime_to_utc('2021-08-11T01:55:05-04:00'))
+    @mock.patch("tap_shopify.streams.base.Stream.update_bookmark")
+    @mock.patch("tap_shopify.streams.base.Stream.get_query_params")
+    @mock.patch("singer.utils.now", return_value=strptime_to_utc(NOW_TIME))
+    def test_interrupted_sync(self, mock_now, mock_get_query_params, mock_update_bookmark, mock_get_bookmark, mock_call_api):
+        """Verify if sync is interrrupted twice in a row then since_id and updated_at_max keys are not deleted from the state"""
+        mock_call_api.side_effect = Exception("Dummy exception...")
+        Context.state = {"bookmarks": {
+            "currently_sync_stream": 'customers',
+            "customers": {
+                "updated_at": "2021-03-27T00:00:00.000000Z",
+                "since_id": 15,
+                "updated_at_max": "2021-04-26T00:00:00.000000Z"}}}
+
+        try:
+            customers = list(CUSTOMER_OBJECT.get_objects())
+        except:
+            pass
+
+        # Verify keys exist
+        self.assertIn("since_id", Context.state["bookmarks"]["customers"])
+        self.assertIn("updated_at_max", Context.state["bookmarks"]["customers"])
+
+        # Verify key values are as expected
+        self.assertEqual(Context.state["bookmarks"]["customers"]['since_id'], 15)
+        self.assertEqual(Context.state["bookmarks"]["customers"]['updated_at_max'], "2021-04-26T00:00:00.000000Z")
+
