af769c24088a20970b1547a46c0b08cf0870d59b
Rohan Hejmadi <rhejmadi@talend.com>
2023-06-14 04:35:43 +0000

    made changes based on review

--- tests/unittests/test_error_handling.py
+++ tests/unittests/test_error_handling.py
@@ -3,18 +3,55 @@ from unittest import mock
 from urllib.error import URLError
 import tap_shopify
 from tap_shopify.streams.transactions import Transactions
+from tap_shopify.streams.orders import Orders
 from tap_shopify.context import Context
 from parameterized import parameterized
 
 class TestShopifyConnectionResetErrorHandling(unittest.TestCase):
 
+    @mock.patch("time.sleep")
+    @mock.patch("pyactiveresource.activeresource.ActiveResource.find")
+    def test_check_access_handle_timeout_error(self, mocked_find, mocked_sleep):
+        '''
+        Test retry handling of URLError
+        '''
+
+        # mock 'find' and raise URLError
+        mocked_find.side_effect = URLError('<urlopen error [Errno 104] Connection reset by peer>')
+
+        # initialize class
+        stream = Orders()
+
+        with self.assertRaises(URLError):
+            stream.call_api({})
+
+        self.assertEqual(mocked_find.call_count, 5)
+
+    @mock.patch("time.sleep")
+    @mock.patch("pyactiveresource.activeresource.ActiveResource.find")
+    def test_check_access_handle_connection_reset_error(self, mocked_find, mocked_sleep):
+        '''
+        Test retry handling of ConnectionResetError
+        '''
+
+        # mock 'find' and raise IncompleteRead error
+        mocked_find.side_effect = ConnectionResetError
+
+        # initialize class
+        locations = Transactions()
+
+        with self.assertRaises(ConnectionResetError):
+            locations.replication_object.find()
+
+        self.assertEqual(mocked_find.call_count, 5)
+
     @parameterized.expand([
-       (URLError, URLError('<urlopen error [Errno 104] Connection reset by peer>'), 'orders', 'call_api({})', 5),
-       (ConnectionResetError, ConnectionResetError, 'transactions', 'replication_object.find()', 5),
+       (URLError('<urlopen error [Errno 104] Connection reset by peer>'), Orders().call_api, 5),
+       (ConnectionResetError(''), Transactions().replication_object.find, 5),
     ])
     @mock.patch("time.sleep")
     @mock.patch("pyactiveresource.activeresource.ActiveResource.find")
-    def test_check_access(self, error_type, error, stream_name, func, expected_retries, mocked_find, mocked_sleep):
+    def test_check_access(self, error, func, expected_retries, mocked_find, mocked_sleep):
         '''
         Test retry handling of URLError and ConnectionResetError
         '''
@@ -22,12 +59,8 @@ class TestShopifyConnectionResetErrorHandling(unittest.TestCase):
         # mock 'find' and raise appropriate error
         mocked_find.side_effect = error
 
-        #initialize class
-        stream = Context.stream_objects[stream_name]()
-        full_function = 'stream.' + func
-
-        with self.assertRaises(error_type):
-           exec(full_function)
+        with self.assertRaises(type(error)):
+           func({})
 
         # verify we backoff expected number of times
         self.assertEqual(mocked_find.call_count, expected_retries)
