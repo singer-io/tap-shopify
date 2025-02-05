e3d7819885f43f2f9b1fed31c24ef1b91082d985
Prijen Khokhani <88327452+prijendev@users.noreply.github.com>
2025-02-05 18:00:55 +0530

    Update bookmark logic for transactions and order_refunds (#197)
    * Update bookmark logic
    
    * Update changelog
    
    * Resolve comments

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages
 
 setup(
     name="tap-shopify",
-    version="2.0.1",
+    version="2.0.2",
     description="Singer.io tap for extracting Shopify data",
     author="Stitch",
     url="http://github.com/singer-io/tap-shopify",
