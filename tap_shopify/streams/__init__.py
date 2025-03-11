3d41f499f7abc22a339addc551b7f84632f0090e
Sourabh Gandhi <sgandhi@talend.com>
2025-03-11 05:09:04 +0000

    Remove reference for the collects and custom collections

--- tap_shopify/streams/__init__.py
+++ tap_shopify/streams/__init__.py
@@ -5,8 +5,6 @@ import tap_shopify.streams.order_refunds
 import tap_shopify.streams.metafields
 import tap_shopify.streams.transactions
 import tap_shopify.streams.products
-import tap_shopify.streams.collects
-import tap_shopify.streams.custom_collections
 import tap_shopify.streams.locations
 import tap_shopify.streams.inventory_levels
 import tap_shopify.streams.inventory_items
