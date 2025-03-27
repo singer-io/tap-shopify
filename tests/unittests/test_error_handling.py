797175fec0ac8ea62a121604cb470f9427f30241
Sourabh Gandhi <sgandhi@talend.com>
2025-03-27 11:37:47 +0000

    update unit test cases

--- tests/unittests/test_error_handling.py
+++ tests/unittests/test_error_handling.py
@@ -1,45 +1,49 @@
 import unittest
-from unittest import mock
+import http.client
+import socket
+from parameterized import parameterized
+import pyactiveresource
+import simplejson
 from urllib.error import URLError
-import tap_shopify
-from tap_shopify.streams.transactions import Transactions
-from tap_shopify.streams.orders import Orders
-from tap_shopify.context import Context
+from unittest.mock import patch, MagicMock
+import itertools
+from tap_shopify.streams.products import Products
+from tap_shopify.streams.base import ShopifyAPIError
 
-class TestShopifyConnectionResetErrorHandling(unittest.TestCase):
+class MockResponse:
+    def __init__(self, msg, url, code):
+        self.msg = msg
+        self.url = url
+        self.code = code
 
-    @mock.patch("time.sleep")
-    @mock.patch("pyactiveresource.activeresource.ActiveResource.find")
-    def test_check_access_handle_timeout_error(self, mocked_find, mocked_sleep):
-        '''
-        Test retry handling of URLError
-        '''
+class TestShopifyErrorHandling(unittest.TestCase):
 
-        # mock 'find' and raise URLError
-        mocked_find.side_effect = URLError('<urlopen error [Errno 104] Connection reset by peer>')
+    @parameterized.expand([
+        ["http_incompleteread_error", lambda: http.client.IncompleteRead(10), http.client.IncompleteRead],
+        ["connection_reset_error", lambda: ConnectionResetError("Connection reset by peer"), ConnectionResetError],
+        ["shopify_api_error", lambda: ShopifyAPIError("Shopify API error"), ShopifyAPIError],
+        ["pyactiveresource_connection_error", lambda: pyactiveresource.connection.Error("Resource connection error with timed out"), pyactiveresource.connection.Error],
+        ["socket_timeout_error", lambda: socket.timeout("The read operation timed out"), TimeoutError],
+        ["server_error", lambda: pyactiveresource.connection.ServerError(MockResponse("Server error", "https://shopify.com", 500)), pyactiveresource.connection.ServerError],
+        ["formats_error", lambda: pyactiveresource.formats.Error("Format error"), pyactiveresource.formats.Error],
+        ["json_decode_error", lambda: simplejson.scanner.JSONDecodeError("JSON decode error", "doc", 0), simplejson.scanner.JSONDecodeError],
+        ["url_error", lambda: URLError("URL error"), URLError]
+    ])
+    @patch("shopify.GraphQL")
+    def test_api_errors(self, name, error_fn, expected_exception, mock_graphql):
+        """Test handling of different API errors"""
 
-        # initialize class
-        stream = Orders()
+        side_effect = error_fn()  # Call the lambda to create the exception instance
 
-        with self.assertRaises(URLError):
-            stream.call_api({})
+        with self.subTest(name=name, side_effect=side_effect, expected_exception=expected_exception):
+            # Mock GraphQL execute method to simulate errors
+            mock_graphql_instance = mock_graphql.return_value
+            mock_graphql_instance.execute = MagicMock(side_effect=itertools.repeat(side_effect, 5))
 
-        self.assertEqual(mocked_find.call_count, 5)
+            obj = Products()
+            obj.data_key = "products"
 
-    @mock.patch("time.sleep")
-    @mock.patch("pyactiveresource.activeresource.ActiveResource.find")
-    def test_check_access_handle_connection_reset_error(self, mocked_find, mocked_sleep):
-        '''
-        Test retry handling of ConnectionResetError
-        '''
+            with self.assertRaises(expected_exception):
+                obj.call_api(query_params={})
 
-        # mock 'find' and raise IncompleteRead error
-        mocked_find.side_effect = ConnectionResetError
-
-        # initialize class
-        stream = Transactions()
-
-        with self.assertRaises(ConnectionResetError):
-            stream.replication_object.find()
-
-        self.assertEqual(mocked_find.call_count, 5)
+            self.assertEqual(mock_graphql_instance.execute.call_count, 5)
--- tests/unittests/test_locations.py
+++ tests/unittests/test_locations.py
@@ -1,43 +1,76 @@
 import unittest
 from unittest import mock
+from datetime import datetime
+from dateutil.tz import tzlocal
 from singer import utils
-from singer.utils import strptime_to_utc, strftime
+from singer.utils import strptime_to_utc
 from tap_shopify.context import Context
 
 LOCATIONS_OBJECT = Context.stream_objects['locations']()
 
-
-class Locations():
-    def __init__(self, id, updated_at):
-        self.id = id
-        self.updated_at = updated_at
-
-    def to_dict(self):
-        return {"id": self.id, "updated_at": self.updated_at}
-
-
-LOCATION_1 = Locations("i11", "2021-08-11T01:57:05-04:00")
-LOCATION_2 = Locations("i12", "2021-08-12T01:57:05-04:00")
-LOCATION_3 = Locations("i21", "2021-08-13T01:57:05-04:00")
-LOCATION_4 = Locations("i22", "2021-08-14T01:57:05-04:00")
-
+sample_response = {
+  "edges": [
+    {
+      "node": {
+        "id": "gid://shopify/Location/62030610598",
+        "createdAt": "2021-04-15T20:28:09Z",
+        "updatedAt": "2021-04-15T20:28:09Z"
+      }
+    },
+    {
+      "node": {
+        "id": "gid://shopify/Location/64149061798",
+        "createdAt": "2021-07-29T08:29:10Z",
+        "updatedAt": "2023-02-15T19:30:38Z"
+      }
+    },
+    {
+      "node": {
+        "id": "gid://shopify/Location/64149127334",
+        "createdAt": "2021-07-29T08:30:03Z",
+        "updatedAt": "2023-02-15T19:30:38Z"
+      }
+    },
+    {
+      "node": {
+        "id": "gid://shopify/Location/64149160102",
+        "createdAt": "2021-07-29T08:34:56Z",
+        "updatedAt": "2023-02-15T19:30:37Z"
+      }
+    }
+  ],
+  "pageInfo": {
+    "endCursor": "mock_cursor",
+    "hasNextPage": False
+  }
+}
 
 class TestLocations(unittest.TestCase):
-    @mock.patch("tap_shopify.streams.base.Stream.update_bookmark")
-    @mock.patch("tap_shopify.streams.base.Stream.get_bookmark")
-    @mock.patch("tap_shopify.streams.locations.Locations.get_locations_data")
-    def test_sync(self, mock_get_locations_data, mock_get_bookmark, mock_update_bookmark):
+  @mock.patch("tap_shopify.streams.base.Stream.update_bookmark")
+  @mock.patch("tap_shopify.streams.base.Stream.get_bookmark")
+  @mock.patch("tap_shopify.streams.locations.Locations.call_api")
+  @mock.patch('tap_shopify.streams.base.utils.now', return_value=datetime(2021, 7, 30, 0, 0, tzinfo=tzlocal()))
+  def test_sync(self, mock_now, mock_call_api, mock_get_bookmark, mock_update_bookmark):
+    expected_sync = [
+      {
+        "id": "gid://shopify/Location/64149127334",
+        "createdAt": "2021-07-29T08:30:03Z",
+        "updatedAt": "2023-02-15T19:30:38Z"
+      },
+      {
+        "id": "gid://shopify/Location/64149160102",
+        "createdAt": "2021-07-29T08:34:56Z",
+        "updatedAt": "2023-02-15T19:30:37Z"
+      }
+    ]
+    mock_call_api.return_value = sample_response
+    mock_get_bookmark.return_value = strptime_to_utc("2021-07-29T08:30:03.000000Z")
 
-        expected_sync = [LOCATION_3.to_dict(), LOCATION_4.to_dict()]
-        mock_get_locations_data.return_value = [LOCATION_1, LOCATION_2, LOCATION_3, LOCATION_4]
+    actual_sync = list(LOCATIONS_OBJECT.sync())
 
-        mock_get_bookmark.return_value = strptime_to_utc("2021-08-13T01:05:05-04:00")
+    # Verify that only 2 record syncs
+    self.assertEqual(actual_sync, expected_sync)
+    max_bookmark = strptime_to_utc("2021-07-29T08:34:56.000000Z")
 
-        actual_sync = list(LOCATIONS_OBJECT.sync())
-
-        # Verify that only 2 record syncs
-        self.assertEqual(actual_sync, expected_sync)
-        max_bookmark = strptime_to_utc("2021-08-14T01:57:05-04:00")
-
-        # Verify that maximum replication key of all keys is updated as bookmark
-        mock_update_bookmark.assert_called_with(utils.strftime(max_bookmark))
+    # Verify that maximum replication key of all keys is updated as bookmark
+    mock_update_bookmark.assert_called_with(utils.strftime(max_bookmark))
