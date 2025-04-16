d61705f5d968c17693e93d30a92f92df846ebcc5
vishalpachpinde <vpachpinde@talend.com>
2025-04-16 15:26:30 +0530

    added merchantrequests

--- tap_shopify/streams/fulfillment_orders.py
+++ tap_shopify/streams/fulfillment_orders.py
@@ -325,6 +325,25 @@ class Fulfillmentorders(Stream):
                                                 hasNextPage
                                             }
                                         }
+                                        merchantRequests(first: 250) {
+                                            edges {
+                                                node {
+                                                    id
+                                                    kind
+                                                    message
+                                                    requestOptions
+                                                    responseData
+                                                    sentAt
+                                                    fulfillmentOrder {
+                                                        id
+                                                    }
+                                                }
+                                            }
+                                            pageInfo {
+                                                endCursor
+                                                hasNextPage
+                                            }
+                                        }
                                     }
                                 }
                                 pageInfo {
