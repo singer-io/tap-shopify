fb62ba3d6ab13568ac2980623b2acc6043eb3989
Sourabh Gandhi <sgandhi@talend.com>
2025-08-18 20:20:02 +0000

    update the automatic key name

--- tap_shopify/streams/order_refunds.py
+++ tap_shopify/streams/order_refunds.py
@@ -11,7 +11,7 @@ class OrderRefunds(Stream):
     data_key = "orders"
     child_data_key = "refunds"
     replication_key = "updatedAt"
-    automatic_keys = ["orders"]
+    automatic_keys = ["order"]
 
     # pylint: disable=too-many-locals
     def get_objects(self):
