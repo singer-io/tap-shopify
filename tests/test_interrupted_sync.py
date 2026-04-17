86be78a0ffa21e073bafaf2656a7ff59187cbddc
Nithin <149024913+nk7up@users.noreply.github.com>
2026-04-17 10:55:03 -0400

    removing current_bookmark when no data available, uses previously updated

--- tests/test_interrupted_sync.py
+++ tests/test_interrupted_sync.py
@@ -38,6 +38,9 @@ class InterruptedSyncTest(BaseTapTest):
 
         expected_streams = {'customers',
                             'collections',
+                            #'orders',
+                            # TODO: SAC-30638 - add orders stream back to the test after
+                            # fixing the issue with orders stream bookmark value
                             'events',
                             'products',
                             'transactions'}
@@ -89,6 +92,9 @@ class InterruptedSyncTest(BaseTapTest):
         base_state = {'bookmarks':
                      {'currently_sync_stream': currently_syncing_stream,
                       'customers': first_sync_state.get('bookmarks').get('customers'),
+                      #'orders': first_sync_state.get('bookmarks').get('orders'),
+                      # TODO: SAC-30638 - add orders stream back to the test after 
+                      # fixing the issue with orders stream bookmark value
                       'collections': first_sync_state.get('bookmarks').get('collections'),
                       'events': first_sync_state.get('bookmarks').get('events'),
                       'products': first_sync_state.get('bookmarks').get('products'),
